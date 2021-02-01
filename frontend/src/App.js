cfa4dedb51911d83f35293175a506a7939ffdabe
HiroRin753 <chikuzen.0306@gmail.com>
2021-02-01 23:38:50 +0900

    containerにファイルを作成

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+}from "react-router-dom";
+
+//components
+import {Restaurants} from './containers/Restaurants.jsx';
+import {Foods} from './containers/Foods.jsx';
+import {Orders} from './containers/Orders.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <Switch>
+       <Route  //店舗一覧ページ propsとは親・子コンポーネント間でのデータの「受け渡し口」のようなものです。
+       exact //exactというprops（それぞれのcomponentが持つ情報のようなもの）はデフォでfalseになっておりここをtrueにする(exact={true}と明示しなくても、exactとするだけでOK)ことで、PATHの完全一致の場合にのみコンポーネントをレンダリングする。
+       path = '/restaurants' > 
+        <Restaurants/>
+       </Route>
+
+       <Route  //フード一覧ページ
+        exact
+         path = "/foods">
+        <Foods/>
+       </Route>
+
+       <Route //注文ページ
+        exact
+         path = "/orders">
+        <Orders/>
+       </Route>
+     </Switch>
+   </Router>
   );
 }
 
