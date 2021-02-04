5e77a54a7db298855f00576ce88a6c58868bf658
HiroRin753 <chikuzen.0306@gmail.com>
2021-02-04 18:37:52 +0900

    トップ画面編集

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,9 +1,33 @@
 import React, {Fragment, useEffect} from 'react';
+import styled from 'styled-components';//このようにすることで、styled-componentsを使うことができる
 
+//apis
 import { fetchRestaurants} from '../apis/restaurants'; 
 
-export const Restaurants = () => {
 
+//images
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+display: flex;
+justify-content: flex-start;
+padding: 8px 32px;
+`;//ここではstyled.divでdivElementに対してスタイルをあてている。ここではページの一番上部にあるヘッダー全体を定義している
+
+const MainLogoImage = styled.img`
+height: 90px;
+`;//ヘッダーのロゴ
+
+const MainCoverImageWrapper = styled.div`
+text-align: center;
+`;
+
+const MainCover = styled.img`
+height: 600px;
+`;//トップページ真中のメイン画像
+
+export const Restaurants = () => {
   useEffect(() => {
     fetchRestaurants()
     .then((data)=>
@@ -13,7 +37,12 @@ export const Restaurants = () => {
   
     return(
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src = {MainLogo} alt = "main logo"/>
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src = {MainCoverImage} alt = "main cover"/>
+      </MainCoverImageWrapper>
     </Fragment>
   )
 }
